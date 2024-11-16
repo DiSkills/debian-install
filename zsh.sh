7f1a9e246490cec987f89d1036cc1add19f42096
DiSkills <anti.python.fastapi@gmail.com>
2024-11-16 20:02:22 +0300

    Added zsh installation

---
+++ zsh.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Installing zsh
+echo "Installing zsh..."
+sudo apt install -y zsh
+echo "[+] Zsh has been installed successfully"
+
+# Installing oh-my-zsh
+echo "Installing oh-my-zsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "[+] oh-my-zsh has been installed successfully"
